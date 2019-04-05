3c8e3081d9b97adb419cbd939a2d92d399fd7fd6
Artem Borisov <dedsa2002@gmail.com>
2019-04-05 19:52:44 +0530

    Enable some UX optimizations from stock

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -233,3 +233,7 @@ ro.surface_flinger.use_smart_90_for_video=true
 ro.surface_flinger.set_display_power_timer_ms=10000
 ro.surface_flinger.set_touch_timer_ms=5000
 ro.surface_flinger.set_idle_timer_ms=9000
+
+# UI
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.use_fifo_ui=1
