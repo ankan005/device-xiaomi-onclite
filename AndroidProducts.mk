1b4fd2c1fee89b55531fdfae6d1d874e3c2c794f
Ankan005 <Ankan005@gmail.com>
2019-12-29 15:45:00 +0000

    bringup havoc

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,5 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_onclite.mk
+    $(LOCAL_DIR)/havoc_onclite.mk
+
+COMMON_LUNCH_CHOICES := \
+    havoc_onclite-userdebug
--- havoc_onclite.mk
+++ havoc_onclite.mk
@@ -13,13 +13,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, device/xiaomi/onclite/device.mk)
 
 # Inherit some AEX stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Bootanimation res
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_onclite
+PRODUCT_NAME := havoc_onclite
 PRODUCT_DEVICE := onclite
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
